ac969c0dd74a2558a8b9f32d1e17b25add39f80d
Christopher Lucas Leyva <168372093+ChristopherLeyva@users.noreply.github.com>
2024-12-13 02:48:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
 const express = require('express');
-const port = 3000;
-const mysql = require('mysql');
-const mysql2 = require('mysql2/promise');
+const mysql = require('mysql2/promise'); // O mysql en lugar de mysql2 si no usas promesas
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const port = 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://3.85.51.133', // Ajusta este valor a tu dominio permitido
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
 app.use(bodyParser.json());
-
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
-
-/Formulario/
-
-var conexion = mysql.createConnection({
+const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'mi_base_datos'
 });
-
-conexion.connect(function(err) {
-    if (err) {
-        console.error('Error conectando a la base de datos: ' + err.stack);
-        return;
-    }
-    console.log('Conectado a la base de datos.');
-});
-
 
 conexion.connect((error) => {
     if (error) {
@@ -42,8 +29,7 @@ conexion.connect((error) => {
     }
 });
 
-
-app.post('/registro', (req, res) => {
+app.post('/registro', async (req, res) => {
     const { nombre, apellido, telefono, correo, consulta } = req.body;
 
     console.log('Datos recibidos:', req.body); // Verifica los datos recibidos
@@ -52,17 +38,16 @@ app.post('/registro', (req, res) => {
         return res.status(400).json({ message: 'Todos los campos son obligatorios' });
     }
 
-    const query = 'INSERT INTO usuarios (Nombre, Apellido, Telefono, Correo,Consulta) VALUES (?, ?, ?, ? ,?)';
-    conexion.query(query, [nombre, apellido, telefono, correo, consulta], (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ message: 'Error al registrar el usuario' });
-        } else {
-            res.status(200).json({ message: 'Usuario registrado exitosamente' });
-        }
-    });
+    try {
+        const query = 'INSERT INTO usuarios (Nombre, Apellido, Telefono, Correo,Consulta) VALUES (?, ?, ?, ? ,?)';
+        const [result] = await conexion.execute(query, [nombre, apellido, telefono, correo, consulta]);
+        res.status(200).json({ message: 'Usuario registrado exitosamente' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error al registrar el usuario' });
+    }
 });
 
 app.listen(port, () => {
     console.log(`Server on port http://3.85.51.133/:${port}`);
-})
+});
