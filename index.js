54bac5874040bee9d40e524aa26aaadaafbae623
Christopher Lucas Leyva <168372093+ChristopherLeyva@users.noreply.github.com>
2024-12-13 02:52:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(cors({
     origin: 'http://3.85.51.133', // Ajusta este valor a tu dominio permitido
