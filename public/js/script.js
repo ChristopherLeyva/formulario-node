1355a8a680111ef7664f68b909ddd7e4fef0175d
Christopher Lucas Leyva <168372093+ChristopherLeyva@users.noreply.github.com>
2024-12-13 08:27:39 -0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,24 @@
- document.getElementById('registroForm').addEventListener('submit', async function (e) {
-            e.preventDefault();
-            const formData = {
-                nombre: document.getElementById('nombre').value,
-                apellido: document.getElementById('apellido').value,
-                telefono: document.getElementById('telefono').value,
-                correo: document.getElementById('correo').value,
-                consulta: document.getElementById('consulta').value
-            };
+document.getElementById('registroForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-            try {
-                const response = await fetch('http://3.85.51.133/registro', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(formData)
-                });
-                const data = await response.json();
-                alert(data.message);
-            } catch (error) {
-                console.error('Error:', error);
-            }
+    const formData = {
+        nombre: document.getElementById('nombre').value,
+        apellido: document.getElementById('apellido').value,
+        telefono: document.getElementById('telefono').value,
+        correo: document.getElementById('correo').value,
+        consulta: document.getElementById('consulta').value
+    };
+
+    try {
+        const response = await fetch('/registro', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData)
         });
+        const data = await response.json();
+        alert(data.message);
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error al enviar el formulario.');
+    }
+});
