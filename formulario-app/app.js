0191d5c014604fb04a4a4f59c9b81bb1028a2e7c
Christopher Lucas Leyva <168372093+ChristopherLeyva@users.noreply.github.com>
2024-12-15 14:14:41 -0500

    Update app.js

--- formulario-app/app.js
+++ formulario-app/app.js
@@ -11,6 +11,6 @@ app.use(express.static('public'));
 app.use('/formulario', formularioRoutes);
 
 const PORT = 80;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
