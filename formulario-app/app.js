2a676244b0c77b692937de4dfad034df682f4853
Christopher Lucas Leyva <168372093+ChristopherLeyva@users.noreply.github.com>
2024-12-15 17:12:45 -0500

    Update app.js

--- formulario-app/app.js
+++ formulario-app/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const formularioRoutes = require('./routes/formulario');
+const cors = require('cors');
 const app = express();
 
+// Configuración del middleware
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Rutas
+app.get('/', (req, res) => {
+    res.redirect('/formulario');
+});
 app.use('/formulario', formularioRoutes);
 
-const PORT = 80;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en http://54.160.191.58:${PORT}`);
+// Iniciar servidor
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
